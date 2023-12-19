a9007e1eef51541af2c2e3afc31ce4cfda6bb668
Yechan0903 Shin <oolo9876@naver.com>
2023-12-19 03:30:11 +0000

    test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         movies: () => [],
-        movie: () => ({title:"Hello", year:2023}),
+        movie: () => ({title:"Hell", year:2023}),
     },
     Mutation: {
         createMovie: (_, {title}) => {
