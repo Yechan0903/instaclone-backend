e7d20b8696b16ffafff9c0deee9c57a3c604338d
Yechan0903 <oolo9876@naver.com>
2024-01-02 19:10:35 +0900

    test12

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
 const server = new ApolloServer({
-  schema
+  schema,
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-server.listen(PORT).then(() => console.log(`Server is running on http://localhost:${PORT}/`))
+server
+  .listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}/`));
