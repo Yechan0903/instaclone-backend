f89bf295f3818d5ad977f78421b5f1d302d3badd
Yechan0903 <oolo9876@naver.com>
2024-01-03 19:17:23 +0900

    4.5

--- users/user.mutations.js
+++ users/user.mutations.js
@@ -1,4 +1,5 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client";
 
 export default {
@@ -10,29 +11,56 @@ export default {
             email,
             password,
         }) => {
-            const existingUser = await client.user.findFirst({
-                where: {
-                    OR: [
-                        {
-                            username
-                        },
-                        {
-                            email,
-                        },
-                    ],
-                },
-            });
-            console.log(existingUser);
-            const uglyPassword = await bcrypt.hash(password, 10);
-            return client.user.create ({
-                data: {
-                    username,
-                    email,
-                    firstName,
-                    lastName,
-                    password: uglyPassword,
-                },
-            });
+            try {
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [
+                            {
+                                username
+                            },
+                            {
+                                email,
+                            },
+                        ],
+                    },
+                });
+                if (existingUser) {
+                    throw new Error("This username/password is already taken.")
+                }
+                const uglyPassword = await bcrypt.hash(password, 10);
+                return client.user.create ({
+                    data: {
+                        username,
+                        email,
+                        firstName,
+                        lastName,
+                        password: uglyPassword,
+                    },
+                });
+            } catch(e) {
+                return e;
+            }
         },
+        login: async(_, {username,password}) => {
+            const user = await client.user.findFirst({where:{username}});
+            if(!user){
+                return {
+                    ok: false,
+                    error: "User not found",
+                };
+            }
+            const passwordOk = await bcrypt.compare(password, user.password);
+            if(!passwordOk) {
+                return {
+                    ok: false,
+                    error: "Incorrect password",
+                };
+            }
+            const token = await jwt.sign({id:user.id}, process.env.SECRET_KEY);
+            return {
+                ok: true,
+                token,
+            };
+        }
     },
 };
